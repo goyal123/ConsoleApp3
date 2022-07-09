eafe51e6abe3d9f126be86b2940780bca0fe4c08
goyal123 <akhilg642@gmail.com>
2022-07-09 12:58:54 +0530

    Second Commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ConsoleApp3
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int age;
+            string name;
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Hello " + name + "!"
+                            + " You can vote");
+            }
+            else
+            {
+                Console.WriteLine("Hello " + name + "!"
+                    + " Sorry you can't vote");
+            }
+
+
         }
     }
 }
